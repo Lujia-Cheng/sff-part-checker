39c6e0cc20d0089c3e1bbcb0bff71f9e14c3b783
Luke Cheng <lukecheng@pitt.edu>
2024-07-08 00:19:57 -0400

    feat: layout ai-chat  and update MUI icon latest version and

--- web-service/app/ui/ai-chat.tsx
+++ web-service/app/ui/ai-chat.tsx
@@ -1,14 +1,66 @@
+// pages/index.tsx
 import { useState } from "react";
-import handler from "../api/ai";
+import { Box, Button, List, TextField } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import StopIcon from "@mui/icons-material/Stop";
 
-export default function AiChat() {
-  const [messages, setMessages] = useState([]);
+import { Content } from "@google/generative-ai";
 
-  const handleSendMessage = (message) => {};
+export default function Home() {
+  const [messageHistory, changeMessageHistory] = useState<Content[]>([]);
+  const [file, setFile] = useState<File | null>(null);
+  const [userText, setUserText] = useState("");
+  const [waitingForServer, setWaitingForServer] = useState(false);
+
+ 
+
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newFile = event.target.files?.[0];
+    if (newFile) {
+      setFile(newFile);
+    }
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    // todo - upload file, send text to server, update messageHistory. see google ai studio example in /web-service/app/test/gemini.js 
+   
+  }
+
+  function handleCancel() {
+    // todo - cancel the request
+  }
 
   return (
-    <div>
-      <h1>TODO AI Chat</h1>
-    </div>
+    <>
+      <List>{/* todo - display chat history  */}</List>
+
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{ mt: 1, display: "flex", gap: "10px" }}
+      >
+        <TextField
+          fullWidth
+          placeholder="Type a message..."
+          value={userText}
+          onChange={(e) => setUserText(e.target.value)}
+        />
+        {waitingForServer ? (
+          <Button variant="contained" type="reset" onClick={handleCancel}>
+            <StopIcon />
+          </Button>
+        ) : (
+          <Button
+            disabled={!userText || waitingForServer}
+            variant="contained"
+            type="submit"
+          >
+            <SendIcon />
+          </Button>
+        )}
+      </Box>
+    </>
   );
 }
